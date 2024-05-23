995dab5e669e9fe8f1dbbe9acab0a2ed8e22ec5b
Muhammad Harith Bin Zainudin <harithzainudin7@gmail.com>
2024-05-23 08:51:07 +0800

    Create helpers.ts

---
+++ clients-and-utilities/ts/helpers.ts
@@ -0,0 +1,17 @@
+function manipulateDate(
+  operation: "add" | "subtract",
+  currentDate: Date,
+  value: number,
+  metric: dayjs.ManipulateType,
+): { dateFormat: Date; dayjsFormat: dayjs.Dayjs } {
+  const dayjsDate = dayjs(currentDate);
+
+  let newDate: dayjs.Dayjs;
+
+  if (operation === "add") newDate = dayjsDate.add(value, metric);
+  else newDate = dayjsDate.subtract(value, metric);
+
+  return { dateFormat: new Date(newDate.format()), dayjsFormat: newDate };
+}
+
+export { manipulateDate };
